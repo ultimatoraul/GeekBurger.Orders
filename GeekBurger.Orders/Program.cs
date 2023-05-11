5f823ce9b410fe36f1411c463bc337e06476bb7f
Raul Pires <raulpires@outlook.com.br>
2023-05-10 22:40:04 -0300

    Context adicionado;

--- GeekBurger.Orders/Program.cs
+++ GeekBurger.Orders/Program.cs
@@ -1,5 +1,12 @@
 using GeekBurger.Orders.Repository;
 using GeekBurger.Orders.Service;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +16,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc();
+builder.Services.AddAutoMapper(typeof(OrdersDbContext));
+
+builder.Services.AddDbContext<OrdersDbContext>
+    (o => o.UseInMemoryDatabase("geekburger-orders"));
 
 builder.Services.AddTransient<IOrdersRepository, OrdersRepository>();
 builder.Services.AddTransient<IOrderChangedEventRepository, OrderChangedEventRepository>();
@@ -32,6 +45,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<OrdersDbContext>();
+    context.Database.EnsureCreated();
+}
 
 app.Run();
